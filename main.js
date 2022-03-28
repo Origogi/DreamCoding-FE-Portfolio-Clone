6e7b322e2016d62810ee67d516e88cf4a263c7e6
origogi <35194820+Origogi@users.noreply.github.com>
2022-03-28 21:53:52 +0900

    JS add contact me

--- main.js
+++ main.js
@@ -14,9 +14,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
+const clickListener = (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
@@ -26,4 +24,12 @@ navbarMenu.addEventListener("click", (event) => {
 
   const scrollTo = document.querySelector(link);
   scrollTo.scrollIntoView({ behavior: "smooth" });
-});
+};
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", clickListener);
+
+// Handle scrolling when tapping on the contact me
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", clickListener);
